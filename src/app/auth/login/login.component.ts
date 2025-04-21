dda44ef786506144cb50e6a20653577be8265e16
Matheus Oliveira da Silva <matheus.6148@gmail.com>
2025-04-21 07:04:36 -0300

    feat: (login) add backend request and HTTP error validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,6 +4,18 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { HttpClient, provideHttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { catchError, finalize, tap, throwError, of } from 'rxjs';
+
+interface LoginResponse {
+  token: string;
+  // user: {
+  //   id: number;
+  //   name: string;
+  //   email: string;
+  // };
+}
 
 @Component({
   standalone: true,
@@ -20,8 +32,16 @@ import { MatCardModule } from '@angular/material/card';
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  isLoading = false;
+  errorMessage: string | null = null;
 
-  constructor(private fb: FormBuilder) {
+  private apiUrl = 'http://localhost:8080/auth/login';
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
@@ -29,11 +49,43 @@ export class LoginComponent {
   }
 
   onSubmit() {
-    if (this.loginForm.valid) {
 
-      console.log(this.loginForm.value);
-    } else {
-      console.log('Formulário inválido');
+    this.loginForm.markAllAsTouched();
+
+
+    if (this.loginForm.invalid) {
+      this.errorMessage = 'Por favor, preencha os campos corretamente.'
+      return;
     }
+
+    this.isLoading = true;
+    this.errorMessage = null;
+    const credentials = this.loginForm.value;
+
+    this.http.post<LoginResponse>(this.apiUrl, credentials)
+    .pipe(
+      tap((response) => {
+        localStorage.setItem('token', response.token);
+        //localStorage.setItem('user', JSON.stringify(response.user));
+        console.log('Token saved to localStorage');
+
+        this.router.navigate(['/home']);
+      }),
+      catchError((error: HttpErrorResponse) => {
+        console.error('Login failed', error);
+
+        if (error.status === 401 || error.status === 400) {
+          this.errorMessage = 'Credenciais inválidas.';
+        } else {
+          this.errorMessage = 'Ocorreu um erro durante o login. Tente novamente mais tarde';
+        }
+
+        return of(null);
+      }),
+      finalize(()=> {
+        this.isLoading = false;
+      })
+    )
+    .subscribe();
   }
 }
