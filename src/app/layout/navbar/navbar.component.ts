97ebca11c5c9672e77ad89fe003adaff275e46a4
Matheus Oliveira da Silva <matheus.6148@gmail.com>
2025-04-21 11:45:00 -0300

    feat: update navbar component with improved layout and icons; enhance styling and add profile menu

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -4,12 +4,21 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [
-    MatButtonModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatListModule
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
